14cb89cd6512bc55dcff91ccb3d3389ce20052a1
Shigeki Masuda <macha3162@gmail.com>
2017-08-02 17:41:39 +0900

    add heroku settings.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,17 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379', namespace: 'kaopass_sidekiq' }
-end
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = {url: ENV['REDIS_URL']}
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379', namespace: 'kaopass_sidekiq' }
+  Sidekiq.configure_client do |config|
+    config.redis = {url: ENV['REDIS_URL']}
+  end
+else
+  Sidekiq.configure_server do |config|
+    config.redis = {url: 'redis://localhost:6379', namespace: 'kaopass_sidekiq'}
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = {url: 'redis://localhost:6379', namespace: 'kaopass_sidekiq'}
+  end
 end
