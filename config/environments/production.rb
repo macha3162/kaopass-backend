e684cbd5c4e03e580fe9043a353701b2cce0d229
Shigeki Masuda <macha3162@gmail.com>
2017-08-02 18:06:33 +0900

    update.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.use Rack::Auth::Basic do |username, password|
+    username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+  end
 end
