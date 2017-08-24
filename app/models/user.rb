73996d8d8d4dd7e28c21355fbdf6dca850095e07
Shigeki Masuda <macha3162@gmail.com>
2017-08-24 17:16:49 +0900

    add session_ids

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :signatures, dependent: :destroy
   has_many :visit_histories, dependent: :destroy
   has_many :session_histories, dependent: :destroy
+  has_many :sessions, :through => :session_histories
 
   after_create :visit
 
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :visit_count
+  attributes :id, :name, :visit_count, :session_ids
   has_many :photos
   has_many :signatures
 
   def visit_count
     object.visit_histories.count if object.present?
+  end
+
+  def session_ids
+    object.sessions.pluck(:number)
   end
 end
